a7367b8da39ebcf03f6f1d32706fec89c47ff631
Ning_0 <ning_0@petalmail.com>
2025-06-15 16:54:46 +0800

    合并最新

--- vueProject/vite.config.js
+++ vueProject/vite.config.js
@@ -18,7 +18,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server:{  
+  server:{
+    host: '0.0.0.0', 
+    https: {
+      key: fs.readFileSync(path.resolve("D:/code/Paper-Manage-System/vueProject/cert", 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve("D:/code/Paper-Manage-System/vueProject/cert", 'localhost.pem')),
+    },   
     proxy:{
       '/api': {
         target: utils.urlnoProxy, // 你的 Spring Boot 后端地址
